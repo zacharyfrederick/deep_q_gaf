b9c7cdab6dc7d7036351553652016bebad6aab24
Zach Frederick <zachfred62@gmail.com>
2020-03-07 05:23:29 -0500

    i am stupid - charles leclerc

--- lib/stock_env.py
+++ lib/stock_env.py
@@ -110,7 +110,7 @@ class StockEnv(gym.Env):
             self.pm.open_position(action, self.clock.index)
 
             reward *= self.REWARD_MULT
-            if self.reward == np.nan:
+            if reward == np.nan:
                 done = True
 
         frame = self.dm.get_frame() if not done else self.first_frame
