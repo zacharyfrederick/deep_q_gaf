68773a242c7160f9fc15a501eb09efeac01b308e
Zach Frederick <zachfred62@gmail.com>
2020-03-11 00:24:18 -0400

    something

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
+
 gsutil cp 'gs://zachf632_senior_project/data.zip' .;
 apt get unzip;
 unzip data.zip;
 rm -r data.zip;
-cd unzip;
+cd data
 gsutil cp 'gs://zachf632_senior_project/concat.zip' .;
 unzip concat.zip;
 rm concat.zip;
-sudo apt install python3;
-pip install 'requirements.txt'
+cd '../../';
+git clone 'https://github.com/zacharyfrederick/Janet.git';
 cd 'lib';
 
 
