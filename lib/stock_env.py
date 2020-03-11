2dee2a5cb2a25e3371bdc65af7cefd53c7674e16
Zach Frederick <zachfred62@gmail.com>
2020-03-11 03:09:25 -0400

    something

--- lib/stock_env.py
+++ lib/stock_env.py
@@ -80,7 +80,7 @@ class StockEnv(gym.Env):
 
         if done == self.dm.INCR_FLAG:
             print('\nCash before increment:' +  str(self.get_cash()))
-            print('Return: {value:0.2f}%'.format(value=(int((self.cash - 100000)/100000) * 100)))
+            print('Return: {value:.2f}%'.format(value=(float((self.cash - 100000)/100000) * 100)))
             self.final_cash_value.append(self.cash)
             len_images, len_symbols = self.dm.increment_symbol()
             self.perm_symbols.append(self.dm.current_symbol)
