aa3df530bd6f647d6f8f5ac64e15e43ddd06d16a
Zach Frederick <zachfred62@gmail.com>
2020-03-11 02:36:03 -0400

    something

--- lib/stock_env.py
+++ lib/stock_env.py
@@ -112,8 +112,9 @@ class StockEnv(gym.Env):
 
     def get_cash(self, value=None):
         source = self.cash if value is None else value
-        return f'${source:,}'
+        return 'Return: ${0:.0f}'.format(source)
 
     def print_returns(self):
         for value in self.final_cash_value:
+
             print(self.get_cash(value))
