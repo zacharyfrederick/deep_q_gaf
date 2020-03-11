85174d2ec857e2c3d38b306acb88d05d109312f3
Zach Frederick <zachfred62@gmail.com>
2020-03-11 05:49:55 -0400

    something

--- lib/stock_env.py
+++ lib/stock_env.py
@@ -91,7 +91,7 @@ class StockEnv(gym.Env):
 
         info = {}
 
-        if reward < -1.0e-04:
+        if self.returns.y.rolling(window=7).mean() < -1.0e-04:
             if action == actions.Actions.BUY:
                 action = actions.Actions.SELL
             elif action == actions.Actions.SELL:
