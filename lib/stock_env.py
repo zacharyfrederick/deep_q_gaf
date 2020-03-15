e56e2c3fc837f1da1d31d5fcbbb29b7e7c89a3c4
Zach Frederick <zachfred62@gmail.com>
2020-03-15 03:31:15 -0400

    something

--- lib/stock_env.py
+++ lib/stock_env.py
@@ -25,7 +25,7 @@ class StockEnv(gym.Env):
         self.previous_action = None
         self.clock = Clock()
         self.dm = DataManager(self.clock)
-        self.pm = PositionManager(self.clock, self.dm, self.cash, 1)
+        self.pm = PositionManager(self.clock, self.dm, self.cash, 10)
         self.symbols = self.dm.get_symbols()
         self.action_space = gym.spaces.Discrete(3)
         self.observation_space = gym.spaces.Box(low=-1, high=1, shape=(4, 30, 180))
