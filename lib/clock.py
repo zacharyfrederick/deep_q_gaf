9714eb6413817c539bf3288390f01f5477f74bdb
Zach Frederick <zachfred62@gmail.com>
2020-03-15 02:12:35 -0400

    something

--- lib/clock.py
+++ lib/clock.py
@@ -13,7 +13,7 @@ class Clock:
         self.index += 1
 
     def done(self):
-        if self.index == self.len_images:
+        if self.index + 1 == self.len_images:
             if self.len_symbols == 1:
                 self.is_done = True
                 return True
