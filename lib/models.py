624f8775a225cbe07f97e1f130b8d947b5d0f252
Zach Frederick <zachfred62@gmail.com>
2020-03-14 21:20:41 -0400

    debug commit please ignore

--- lib/models.py
+++ lib/models.py
@@ -1,5 +1,5 @@
 from keras.layers import Input, Dense, concatenate
-from keras.layers import Activation, Conv2D, Flatten, Dropout, Reshape, Permute, Lambda
+from keras.layers import Activation, Conv3D, Flatten, Dropout, Reshape, Permute, Lambda
 from tensorflow.keras.utils import multi_gpu_model
 from keras.models import Sequential
 import keras as keras
@@ -14,9 +14,9 @@ def flatten_fo_final_size(x, size=512):
     return x
 
 def build_deepmind_model(x):
-    x = Conv2D(32, (8, 8), strides=(4, 4), activation='relu', data_format='channels_first')(x)
-    x = Conv2D(64, (4, 4), strides=(1, 1), activation='relu', data_format='channels_first')(x)
-    x = Conv2D(64, (3, 3), strides=(1, 1), activation='relu', data_format='channels_first')(x)
+    x = Conv3D(32, (8, 8), strides=(4, 4), activation='relu', data_format='channels_first')(x)
+    x = Conv3D(64, (4, 4), strides=(1, 1), activation='relu', data_format='channels_first')(x)
+    x = Conv3D(64, (3, 3), strides=(1, 1), activation='relu', data_format='channels_first')(x)
     return x
 
 def convert_tensors_to_model(x, num_actions, activation):
@@ -44,11 +44,11 @@ def build_combined_model():
 def build_paper_model(num_gpus=None):
     model = Sequential()
     model.add(
-        Conv2D(32, (8, 8), strides=(4, 4), input_shape=(4, 30, 180), activation='relu', data_format='channels_first'))
+        Conv3D(32, (8, 8), strides=(4, 4), input_shape=(4, 30, 180), activation='relu', data_format='channels_first'))
     model.add(Activation('relu'))
-    model.add(Conv2D(64, (4, 4), strides=(1, 1), data_format='channels_first'))
+    model.add(Con3D(64, (4, 4), strides=(1, 1), data_format='channels_first'))
     model.add(Activation('relu'))
-    model.add(Conv2D(64, (3, 3), strides=(1, 1), data_format='channels_first'))
+    model.add(Conv3D(64, (3, 3), strides=(1, 1), data_format='channels_first'))
     model.add(Activation('relu'))
     model.add(Flatten())
     #model.add(Dropout(0.5))
