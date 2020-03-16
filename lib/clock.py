2648135b0750e2d100787124d4deb0d7ebb8a68a
Zach Frederick <zachfred62@gmail.com>
2020-03-16 17:49:15 -0400

    debug commit

--- lib/clock.py
+++ lib/clock.py
@@ -13,7 +13,6 @@ class Clock:
         self.index += 1
 
     def done(self):
-        print(self.index, self.len_images)
         if self.index + 2 == self.len_images:
             if self.len_symbols == 1:
                 self.is_done = True
